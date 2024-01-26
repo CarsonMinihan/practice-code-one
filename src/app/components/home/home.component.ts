ecb1b95cba395452d123e410689cd6094a056b62
Carson Minihan <cdminihan01@gmail.com>
2024-01-26 10:52:14 -0800

    images now display

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor() {}
+  public imagesList = [
+    './assets/images/img1.jpg',
+    './assets/images/img2.jpg',
+    './assets/images/img3.jpg',
+    './assets/images/img4.jpg',
+    './assets/images/img5.jpg',
+    './assets/images/img6.jpg',
+  ]
+   
 
+  ngOnInit(): void {
+    // customElements.define('div', image);
+    // let images = ["assets/images/img1.jpg"]
+  }
 }
